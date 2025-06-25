cca7117d4d1d02748260e27b261b6fd7e1158278
KalvaniAnurag <kalvanianurag@gmail.com>
2025-06-25 23:43:21 +0530

    streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -11,7 +11,7 @@ hours=st.number_input("Hours Studied:",min_value=0.0, step=1.0)
 
 if st.button("Predict"):
     try:
-        data=[[hours]]z
+        data=[[hours]]
         scaled_data=scaler.transform(data)
         prediction=model.predict(scaled_data)
         st.write("Predicted Test Score:",prediction[0])
